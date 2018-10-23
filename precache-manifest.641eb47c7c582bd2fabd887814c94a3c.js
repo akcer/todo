c53359c0f8fec962759dc68cbea38d29ae970cd2
Jarek Hryniewicki <jarhryniewicki@gmail.com>
2018-10-23 09:27:33 +0200

    Updates

---
+++ precache-manifest.641eb47c7c582bd2fabd887814c94a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "4e010bc54ec85537aeb9",
+    "url": "/todo/static/js/main.4e010bc5.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/todo/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4e010bc54ec85537aeb9",
+    "url": "/todo/static/css/main.acb65cff.chunk.css"
+  },
+  {
+    "revision": "0c524300452a43a30a2f0fdda465cfe9",
+    "url": "/todo/index.html"
+  }
+];
